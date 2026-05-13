5fa5eeea659bced4528291ac9c135d2aaf5b9033
Kamal <kamal.farahani90@gmail.com>
2026-05-13 20:24:31 +0330

    feat: add MeditationSession viewset with user filtering and authentication

--- meditation/views.py
+++ meditation/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import permissions, viewsets
 
-# Create your views here.
+from meditation.models import MeditationSession
+from meditation.serializers import MeditationSessionSerializer
+
+
+class MeditationSessionViewSet(viewsets.ModelViewSet):
+    serializer_class = MeditationSessionSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return MeditationSession.objects.filter(user=self.request.user)  # type: ignore[return-value]
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
