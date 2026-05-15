18ef29494a890862761d1fcc90f181a934278cb5
Kamal <kamal.farahani90@gmail.com>
2026-05-15 17:57:43 +0330

    feat: add EmailVerificationToken admin interface with token display and filtering

--- meditation/admin.py
+++ meditation/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from meditation.models import MeditationSession
+from meditation.models import EmailVerificationToken, MeditationSession
 
 
 @admin.register(MeditationSession)
@@ -9,3 +9,11 @@ class MeditationSessionAdmin(admin.ModelAdmin):
     list_filter = ["start_time", "user"]
     search_fields = ["user__username", "user__email"]
     date_hierarchy = "start_time"
+
+
+@admin.register(EmailVerificationToken)
+class EmailVerificationTokenAdmin(admin.ModelAdmin):
+    list_display = ["id", "user", "token", "created_at", "expires_at"]
+    list_filter = ["created_at", "user"]
+    search_fields = ["user__username", "user__email"]
+    date_hierarchy = "created_at"
